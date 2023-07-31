5705c49d89e4d8e38d1dab7a7f93722ecab2bdef
Ehab-Tarek <ehabtarek166@gmail.com>
2023-08-01 02:18:22 +0300

    Start user logic.

--- src/modules/user/controller/user.controller.js
+++ src/modules/user/controller/user.controller.js
@@ -1,8 +1,17 @@
 
 import userModel from "../../../../database/models/user.model.js";
 import bcrypt from "bcrypt";
-export const signUp = (req, res) => {
+export const signUp = async (req, res) => {
     try {
+
+        //NOTE - Check for user registered before
         let { userName, email, password, age, gender, phone } = req.body;
         const hashedPassword = bcrypt.hashSync(password, 10);
-        const user = userModel.insertMany({ userName, emai
+        const user = await userModel.insertMany({ userName, email, password: hashedPassword, age, gender, phone });
+        res.json({ Message: "user Added Successfully", user });
+
+    } catch (err) {
+        res.json({ Message: "ERROR", err });
+    }
+
+}
