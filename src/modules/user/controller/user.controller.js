c552f220386d18139716c4af01e850a077749ef3
Ehab-Tarek <ehabtarek166@gmail.com>
2023-08-01 02:26:18 +0300

    FInish User SignUp .

--- src/modules/user/controller/user.controller.js
+++ src/modules/user/controller/user.controller.js
@@ -1,17 +1,26 @@
 
 import userModel from "../../../../database/models/user.model.js";
 import bcrypt from "bcrypt";
+
+
+
+
+//NOTE - 1-signUp 
 export const signUp = async (req, res) => {
     try {
 
         //NOTE - Check for user registered before
         let { userName, email, password, age, gender, phone } = req.body;
-        const hashedPassword = bcrypt.hashSync(password, 10);
-        const user = await userModel.insertMany({ userName, email, password: hashedPassword, age, gender, phone });
-        res.json({ Message: "user Added Successfully", user });
-
+        const userFounded = await userModel.findOne({ email }).select("-password -_id ");
+        if (userFounded) {
+            res.status(200).json({ Message: "user Already Exist", userFounded });
+        } else {
+            const hashedPassword = bcrypt.hashSync(password, parseInt(process.env.SULTROUND));
+            const user = await userModel.insertMany({ userName, email, password: hashedPassword, age, gender, phone });
+            res.status(200).json({ Message: "user Added Successfully", user });
+        }
     } catch (err) {
-        res.json({ Message: "ERROR", err });
+        res.status(501).json({ Message: "ERROR", err });
     }
 
 }
